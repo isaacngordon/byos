33925d495e76c300996e691d06658e225ae9535a
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-11-25 07:40:31 -0500

    move stuff

--- src/components/Sefariah/Search.tsx
+++ src/components/Sefariah/Search.tsx
@@ -1,6 +1,6 @@
 "use client";
 import {  useState } from "react";
-
+import { dangerParser } from "@/components/Sefariah/DangerousHtml";
 
 export default function Search() {
 
@@ -36,19 +36,3 @@ export default function Search() {
   );
 }
 
-// remove any not allowed html elements, and then return the whole text as dangerously set html span
-function dangerParser(text: string) {
-  const whitelisted_elements = ["span", "b", "i", "u", "br", "big"];
-  const set_of_found_elements = new Set(text.split(/(<\/?[a-z][^>]*>)/i)
-    .filter((element) => whitelisted_elements.includes(element.slice(1, element.length - 1))));
-    console.log("Found:", set_of_found_elements);
-  
-    for (const element of set_of_found_elements) {
-      if (!whitelisted_elements.includes(element.slice(1, element.length - 1))) {
-        // replace open and closing tags with empty strings using regex to ensure no overlapping tags and attributes are removed
-        text = text.replace(new RegExp(`<${element}>`, "g"), "");
-        text = text.replace(new RegExp(`</${element}>`, "g"), "");
-      }
-    }
-    return <div dangerouslySetInnerHTML={{ __html: text }} />;
-}
