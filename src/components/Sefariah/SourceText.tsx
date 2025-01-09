71a1439c41364cb14d391c69d76ab19da97f4b41
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2025-01-09 11:24:00 -0500

    refactor: Rename 'ref' prop to 'reference' and add loading state in SourceText component

--- src/components/Sefariah/SourceText.tsx
+++ src/components/Sefariah/SourceText.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 const default_whitelist = ["span", "b", "i", "u", "br", "big"]
 
 interface SourceTextProps {
-    ref: string;
+    reference: string;
     versions: string[];
     whitelist_html?: string[];
 }
@@ -18,12 +18,16 @@ async function get_all_texts(ref: string, versions: string[]) {
     }));
 }
 
-export default function SourceText({ ref, versions, whitelist_html = default_whitelist }: SourceTextProps) {
+export default function SourceText({ reference, versions, whitelist_html = default_whitelist }: SourceTextProps) {
     const [versions_texts, setVersionTexts] = useState<string[]>([]);
+    const [loading, setLoading] = useState(true);
+
 
     async function getTexts() {
-        const texts = await get_all_texts(ref, versions);
+        setLoading(true);
+        const texts = await get_all_texts(reference, versions);
         setVersionTexts(texts);
+        setLoading(false);
     }
 
     useEffect(()=>{
@@ -34,13 +38,14 @@ export default function SourceText({ ref, versions, whitelist_html = default_whi
     return (
         <div className="border border-gray-300 p-4 rounded-lg bg-gray-700 text-white">
             <div className="flex flex-row justify-between gap-2 mb-2">
-                <h2 className="text-2xl font-bold">{ref}</h2>
-                <Link href={SEFARIA_API_ENDPOINTS.sefaria_site(ref)} target="_blank" className="text-blue-400 text-sm my-auto">
+                <h2 className="text-2xl font-bold">{reference}</h2>
+                <Link href={SEFARIA_API_ENDPOINTS.sefaria_site(reference)} target="_blank" className="text-blue-400 text-sm my-auto">
                     [Source]
                 </Link>
             </div>
             <ul>
-                {versions_texts.map((version, index) => (
+                {loading && <div>Loading...</div>}
+                {!loading && versions_texts.map((version, index) => (
                     <DangerousHtml key={index} text={version} whitelisted_elements={whitelist_html} className="px-2 py-1" />
                 ))}
             </ul>
